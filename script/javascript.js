c461490b74e40a9aef8f4e1d6119f9d10fe13572
Leonardo Campos <leonardo.kiss66@live.com>
2024-01-15 13:14:23 -0300

    update

--- script/javascript.js
+++ script/javascript.js
@@ -1,3 +1,10 @@
+/* EFEITO DA ANIMAÇÃO PARA OS LADOS OU PARA CIMA
+window.SimpleAnime=class{constructor(){this.items=document.querySelectorAll("[data-anime]"),this.init()}animateItems(){this.items.forEach(t=>{const e=Number(t.getAttribute("data-anime"));isNaN(e)||setTimeout(()=>{t.classList.add("anime")},e)})}handleVisibility(){void 0!==document.visibilityState?"visible"===document.visibilityState&&this.animateItems():this.animateItems()}init(){this.handleVisibility=this.handleVisibility.bind(this),this.handleVisibility(),document.addEventListener("visibilitychange",this.handleVisibility)}}
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+*/
+
 // MENU HAMBURGUER NAVBAR
 function initMenuMobile(){
   const btnMobile = document.querySelector(".btn-mobile");
@@ -16,6 +23,29 @@ function initMenuMobile(){
 initMenuMobile();
 // MENU HAMBURGUER NAVBAR//
 
+
+// EFEITO SCROLL SUAVE
+function initScrollSuave(){
+const linksInternos = document.querySelectorAll('.header-menu a[href^="#"]');
+
+  function scrollSuave(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+  
+  linksInternos.forEach((link)=>{
+    link.addEventListener('click', scrollSuave);
+  })
+}
+initScrollSuave();
+// EFEITO SCROLL SUAVE //
+
 // EFEITO MAQUINA DE DIGITAR 
 function initTypeWriter(){
   const titulo = document.querySelectorAll('.js-h1-efeito')
@@ -27,7 +57,7 @@ function initTypeWriter(){
     textoArray.forEach((letra, i)=>{
       setTimeout(function(){
         elemento.innerHTML += letra;
-      }, 110 * i)
+      }, 80 * i)
     })
   }
   
@@ -37,19 +67,80 @@ function initTypeWriter(){
 initTypeWriter();
 // EFEITO MAQUINA DE DIGITAR //
 
-const linksInternos = document.querySelectorAll('.header-menu a[href^="#"]');
-
-function scrollSuave(event){
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
+// EFEITO ANIMASCROLL 
+function initAnimacaoScroll(){
+  const sections = document.querySelectorAll('.js-scroll');
+  const windowMetade = window.innerHeight *0.6;
   
-  section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  });
+  function animaScroll(){
+    sections.forEach((section)=>{
+      const sectionTop = section.getBoundingClientRect().top -windowMetade;
+  
+      if(sectionTop < 0){
+        section.classList.add('ativo');
+      } else{
+        section.classList.remove('ativo');
+      }
+    })
+  }
+  
+  window.addEventListener('scroll', animaScroll);
 }
+initAnimacaoScroll();
+// EFEITO ANIMASCROLL  //
 
-linksInternos.forEach((link)=>{
-  link.addEventListener('click', scrollSuave);
-})
+// EFEITO ANIMA SCROLL PARA OS CARDS DAS ESPECIALIDAES 
+
+function initAnimacaoScroll1() {
+  const liScroll = document.querySelector(".js-li-scroll1");
+  const windowMetade = window.innerHeight * 0.7;
+
+  function animaScroll() {
+    const li = liScroll;
+      const liTop = li.getBoundingClientRect().top - windowMetade;
+      if (liTop < 0) {
+        li.classList.add("ativo");
+      } else {
+        li.classList.remove("ativo");
+      }
+    };
+  window.addEventListener("scroll", animaScroll);
+}
+initAnimacaoScroll1();
+
+function initAnimacaoScroll2() {
+  const liScroll = document.querySelector(".js-li-scroll2");
+  const windowMetade = window.innerHeight * 0.7;
+
+  function animaScroll() {
+    const li = liScroll;
+      const liTop = li.getBoundingClientRect().top - windowMetade;
+      if (liTop < 0) {
+        li.classList.add("ativo");
+      } else {
+        li.classList.remove("ativo");
+      }
+    };
+
+  window.addEventListener("scroll", animaScroll);
+}
+initAnimacaoScroll2();
+
+function initAnimacaoScroll3() {
+  const liScroll = document.querySelector(".js-li-scroll3");
+  const windowMetade = window.innerHeight * 0.7;
+
+  function animaScroll() {
+    const li = liScroll;
+      const liTop = li.getBoundingClientRect().top - windowMetade;
+      if (liTop < 0) {
+        li.classList.add("ativo");
+      } else {
+        li.classList.remove("ativo");
+      }
+    };
+
+  window.addEventListener("scroll", animaScroll);
+}
+initAnimacaoScroll3();
+// EFEITO ANIMA SCROLL PARA OS CARDS DAS ESPECIALIDAES  //
